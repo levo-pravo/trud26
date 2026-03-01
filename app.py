a87b7c4758c2cac31c08a2231cb3ecedd29e151d
levo-pravo 😊 <130217523+levo-pravo@users.noreply.github.com>
2026-03-01 12:17:52 +0000

    0.0.0.0

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ app.router.add_get('/', get_index)
 app.router.add_get('/facts', get_cats)
 
 if __name__ == '__main__':
-    web.run_app(app, host='localhost', port=8080)
+    web.run_app(app, host='0.0.0.0', port=8080)
